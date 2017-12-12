7c930bccfbc1cff42a82c944d15ffbdab7b2f8bc
Truong Nguyen <tmnguyen12@gmail.com>
2017-12-11 23:12:12 -0800

    Added reducers and store

---
+++ frontend/reducers/entities_reducer.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux';
+
+// const entitiesReducer = combineReducers({
+// });
+
+const entitiesReducer = (state = {}, action ) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+};
+
+export default entitiesReducer;
