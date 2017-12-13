01b64080eb41de4766fb0748648f2ca259744b48
Truong Nguyen <tmnguyen12@gmail.com>
2017-12-12 20:30:39 -0800

    Search and Business/ID works

--- frontend/localize.jsx
+++ frontend/localize.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store'; 
+
+import { getBusiness, search } from './util/yelp_api'; 
+
 
 document.addEventListener("DOMContentLoaded", () => {
+  let store = configureStore(); 
   const root = document.getElementById('root');
+
+  window.getBusiness = getBusiness; 
+  window.search = search; 
 
   ReactDOM.render(<h1>LOCALIZER</h1>, root);
 });
