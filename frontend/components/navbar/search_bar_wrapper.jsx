4563c11698e6c1341bd3f2f7c99de32d91fb66d2
Angel Velasco <avelasco920@gmail.com>
2017-12-12 20:48:12 -0800

    add carousel

--- frontend/components/navbar/search_bar_wrapper.jsx
+++ frontend/components/navbar/search_bar_wrapper.jsx
@@ -2,23 +2,10 @@ import React from 'react';
 import SearchFieldWrapper from './search_field_wrapper';
 import Categories from './search_bar_categories';
 
-const style = {
-  searchBarWrapper: {
-    height: '50px',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '1000px',
-    margin: 'auto',
-    padding: '10px',
-    boxSizing: 'border-box'
-  }
-};
-
 class SearchBarWrapper extends React.Component {
   render () {
     return (
-      <div style={style.searchBarWrapper}>
+      <div className="search-bar-wrapper">
         <SearchFieldWrapper />
         <Categories />
       </div>
