8e950651ca0b22f92b352066d7761e593753f80d
Kyle Chen <kyle.h.chen@gmail.com>
2017-12-14 03:03:07 -0800

    Create sign in dropdown

--- frontend/components/navbar/title_session_links.jsx
+++ frontend/components/navbar/title_session_links.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
-import Modal from 'react-modal';
+import AuthDropdown from './auth_dropdown';
 
 class SessionLinks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authVisible: false
+    };
+
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+  }
+
+  toggleDropdown(e) {
+    e.stopPropagation();
+    let authVisible = this.state.authVisible ? false : true;
+    this.setState({ authVisible });
+  }
+
   render() {
+    const authDropdownClass =
+      this.state.authVisible ? "auth-dropdown" : "auth-dropdown hidden";
+
     return (
       <div className='session-links'>
-        <a href="#">sign in</a>
+        <a onClick={ this.toggleDropdown }>
+          sign in
+        </a>
+        <AuthDropdown
+          toggleDropdown={ this.toggleDropdown }
+          class={ authDropdownClass }
+          />
         <a href="#">sign up</a>
       </div>
     );
