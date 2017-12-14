12cfaa6a8116a9c68931f002c2005a56ca729a3a
Angel Velasco <avelasco920@gmail.com>
2017-12-13 16:17:46 -0800

    added mission page

---
+++ frontend/components/homepage/mission_statement_right.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const MissionStatementRight = () => (
+  <div className='mission-statement statement-right'>
+    <div className='mission-right-img'>
+      <div className='mission-right-text-wrapper'>
+        <h1 className='mission-right-title'>Statement 2</h1>
+        <span className='mission-right-text display-text'>
+          Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Proin in elit erat. Vestibulum
+          et enim bibendum, molestie ex non, ullamcorper sapien.
+          Curabitur eu metus mi.
+        </span>
+      </div>
+    </div>
+  </div>
+);
+
+export default MissionStatementRight;
