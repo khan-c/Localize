e32b42f5497c2319cf2b8270eb4aada9b28ef4d8
Angel Velasco <avelasco920@gmail.com>
2017-12-14 00:11:34 -0800

    added autocomplete reducer

--- frontend/actions/search_actions.js
+++ frontend/actions/search_actions.js
@@ -1,69 +1,73 @@
-import { search, fetchAutoComplete } from '../util/yelp_api'; 
+import { search, fetchAutoComplete } from '../util/yelp_api';
 
-export const SEARCH_ALL = 'SEARCH_ALL'; 
+export const SEARCH_ALL = 'SEARCH_ALL';
 export const RECEIVE_NO_SEARCH_RESULTS = 'RECEIVE_NO_SEARCH_RESULTS';
-export const AUTOCOMPLETE = 'AUTOCOMPLETE'; 
+export const AUTOCOMPLETE = 'AUTOCOMPLETE';
+export const RECEIVE_NO_AUTOCOMPLETE = 'RECEIVE_NO_AUTOCOMPLETE';
 
 export const searchAll = results => {
-  // console.log("Search Results", results.config); 
   return(
     {
-      type: SEARCH_ALL, 
+      type: SEARCH_ALL,
       businesses: results.data.data.businesses, // an array of objects
       region: results.data.data.region,
       query: results.config.params
     }
-  ); 
-}; 
+  );
+};
 
 export const autoComplete = results => {
-  // console.log("auto RESUTL", results.data.data); 
   return(
     {
-      type: AUTOCOMPLETE, 
+      type: AUTOCOMPLETE,
       results: results.data.data // an array of objects
     }
-  ); 
-}; 
+  );
+};
 
-//sample query format for search 
+//sample query format for search
 // const defaultQuery = {
-//   term: "plumbing", 
-//   latitude: "37.786882",  
-//   longitude: "-122.399972", 
-//   category: "" 
-// }; 
+//   term: "plumbing",
+//   latitude: "37.786882",
+//   longitude: "-122.399972",
+//   category: ""
+// };
 export const getSearch = query => dispatch => {
   return (
     search(query).then( results => {
-      dispatch(searchAll(results)); 
-    }) 
-  ); 
+      dispatch(searchAll(results));
+    })
+  );
 };
- 
+
 // format of query for autocomplete (notice text and not term)
 // const defaultQuery = {
-//   text: "plumbing", 
-//   latitude: "37.786882",  
-//   longitude: "-122.399972", 
-//   category: "" 
-// }; 
+//   text: "plumbing",
+//   latitude: "37.786882",
+//   longitude: "-122.399972",
+//   category: ""
+// };
 
 export const getAutoComplete = query => dispatch => {
-  console.log("autocomplet eaction", query); 
   return (
     fetchAutoComplete(query).then( results => {
-      console.log("auto result", results); 
-      dispatch(autoComplete(results)); 
-    }) 
-  ); 
+      dispatch(autoComplete(results));
+    })
+  );
 };
 
 const receiveNoResults = () => ({
   type: RECEIVE_NO_SEARCH_RESULTS,
 });
 
-export const clearSearchResults = query => dispatch => (
+const receiveNoAutocomplete = () => ({
+  type: RECEIVE_NO_AUTOCOMPLETE,
+});
+
+export const clearSearchResults = () => dispatch => (
   dispatch(receiveNoResults())
 );
 
+export const clearAutocomplete = () => dispatch => (
+  dispatch(receiveNoAutocomplete())
+);
--- frontend/localize.jsx
+++ frontend/localize.jsx
@@ -4,17 +4,17 @@ import configureStore from './store/store';
 import Root from './components/root';
 require('./assets/stylesheets/application.scss');
 
-import { fetchCurrentUser } from './actions/session_actions';        
-import { getBusiness, search, fetchAutoComplete } from './util/yelp_api'; 
-import { getSearch, getAutoComplete } from './actions/search_actions'; 
-import { showBusiness } from './actions/business_actions'; 
+import { fetchCurrentUser } from './actions/session_actions';
+import { getBusiness, search, fetchAutoComplete } from './util/yelp_api';
+import { getSearch, getAutoComplete, clearAutocomplete } from './actions/search_actions';
+import { showBusiness } from './actions/business_actions';
 
 
 const defaultQuery = {
-  term: "laundry", 
-  latitude: "37.786882",  
-  longitude: "-122.399972", 
-  category: "" 
+  text: "laundry",
+  latitude: "37.786882",
+  longitude: "-122.399972",
+  category: ""
 };
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -23,13 +23,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
   window.getState = store.getState;
   window.fetchCurrentUser = fetchCurrentUser;
-  window.getBusiness = getBusiness; 
-  window.search = search; 
-  window.getSearch = getSearch(defaultQuery); 
-  window.getAutoComplete = getAutoComplete(defaultQuery); 
-  window.showBusiness = showBusiness("blackline-construction-san-francisco"); 
-  window.dispatch = store.dispatch; 
-  window.fetchAutoComplete = fetchAutoComplete; 
+  window.getBusiness = getBusiness;
+  window.search = search;
+  window.getSearch = getSearch;
+  window.clearAutocomplete = clearAutocomplete;
+  window.getAutoComplete = getAutoComplete;
+  window.showBusiness = showBusiness("blackline-construction-san-francisco");
+  window.dispatch = store.dispatch;
+  window.fetchAutoComplete = fetchAutoComplete;
 
   ReactDOM.render(<Root store={store}/>, root);
 });
