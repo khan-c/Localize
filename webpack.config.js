60cfffe6750832eadcd7908dbe47e685c3240407
Kyle Chen <kyle.h.chen@gmail.com>
2017-12-15 01:23:37 -0800

    More fixes to try to push to heroku

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,30 @@ var path = require("path");
 var webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-
 const extractSass = new ExtractTextPlugin({
     filename: "./frontend/style.css",
     disable: process.env.NODE_ENV === "development"
 });
 
-// var plugins = []; // if using any plugins for both dev and production
-// var devPlugins = []; // if using any plugins for development
-//
-// var prodPlugins = [
-//   new webpack.DefinePlugin({
-//     'process.env': {
-//       'NODE_ENV': JSON.stringify('production')
-//     }
-//   }),
-//   new webpack.optimize.UglifyJsPlugin({
-//     compress: {
-//       warnings: true
-//     }
-//   })
-// ];
-//
-// plugins = plugins.concat(
-//   process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-// );
+var plugins = [extractSass]; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
 
 module.exports = {
   context: __dirname,
@@ -35,7 +34,7 @@ module.exports = {
     path: path.resolve(__dirname),
     filename: "./frontend/bundle.js",
   },
-  // plugins: plugins,
+  plugins: plugins,
   module: {
     loaders: [
       {
@@ -63,7 +62,4 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx", "*"]
   },
-  plugins: [
-    extractSass,
-  ],
 };
