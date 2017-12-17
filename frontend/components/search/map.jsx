6e9f4cec1c41234e7b7ba024d15e89ddefc9c405
Truong Nguyen <tmnguyen12@gmail.com>
2017-12-16 21:51:04 -0800

    Map centers and zooms in at appropriate level

--- frontend/components/search/map.jsx
+++ frontend/components/search/map.jsx
@@ -1,5 +1,5 @@
 import React from 'react'; 
-
+import MarkerManager from '../../util/marker_manager'; 
 
 class Map extends React.Component {
   componentDidMount() {
@@ -9,61 +9,117 @@ class Map extends React.Component {
         lat: 37.773972,
         lng: -122.431291
       }, // San Francisco coords
-      zoom: 13
+      zoom: 14
     };
+    debugger 
+    if (this.props.region){
+      mapOptions.center["lat"] = this.props.region.center.latitude; 
+      mapOptions.center["lng"] = this.props.region.center.longitude; 
+    }
+
+  debugger 
+    const map = this.refs.map; 
+    //creates new map 
+    var gmap = new google.maps.Map(map, mapOptions);    
+    let infoWindow = new google.maps.InfoWindow;
+    gmap["markers"] = []; 
+    if (this.props.businesses) {
+      this.props.businesses.forEach( (business,idx) => {
+        debugger 
+        var latLng = new google.maps.LatLng(business.coordinates.latitude, business.coordinates.longitude);
+        var marker = new google.maps.Marker({
+          position: latLng,
+          map: gmap
+        });
+      gmap["markers"].push(marker);   
+      }); 
+    }
+    // this.MarkerManager = new MarkerManager(this.gmap, this.handleMarkerClick.bind(this));
+    // this.MarkerManager.updateMarkers(this.props.businesses);k
+    
+    // componentWillReceiveProps(){
+      
+    // }
+
+    // var latLng = new google.maps.LatLng("37.803970", "-122.413146");
+    // var marker = new google.maps.Marker({
+    //   position: latLng,
+    //   map: gmap
+    // });
+    
+    // var latLng2 = new google.maps.LatLng("37.788693", "-122.417545");
+    // var marker2 = new google.maps.Marker({
+    //   position: latLng2,
+    //   map: gmap
+    // });
+
+    // gmap["markers"] = [marker, marker2]; s
+    // Try HTML5 geolocation.
+    // if (navigator.geolocation) {
+    //   navigator.geolocation.getCurrentPosition(function(position) {
+    //     var pos = {
+    //       lat: position.coords.latitude,
+    //       lng: position.coords.longitude
+    //     };
+    //     console.log(pos); 
+    //     infoWindow.setPosition(pos);
+    //     infoWindow.setContent('Location found.');
+    //     infoWindow.open(map);
+    //     map.setCenter(pos);
+    //   }, function() {
+    //     handleLocationError(true, infoWindow, map.getCenter());
+    //   });
+    // } else {
+    //   // Browser doesn't support Geolocation
+    //   handleLocationError(false, infoWindow, map.getCenter());
+    // }
+  
+
+    // function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    //   infoWindow.setPosition(pos);
+    //   infoWindow.setContent(browserHasGeolocation ?
+    //                         'Error: The Geolocation service failed.' :
+    //                         'Error: Your browser doesn\'t support geolocation.');
+    //   infoWindow.open(map);
+    // }
+  }
+
+  componentWillReceiveProps(newProps){
+    
+    const mapOptions = {
+      center: {
+        lat: 37.773972,
+        lng: -122.431291
+      }, // San Francisco coords
+      zoom: 14
+    };
+
+    if (this.props.region){
+      mapOptions.center["lat"] = this.props.region.center.latitude; 
+      mapOptions.center["lng"] = this.props.region.center.longitude; 
+    }
 
     const map = this.refs.map; 
     //creates new map 
-    let gmap = new google.maps.Map(map, mapOptions);    
-    let infoWindow = new google.maps.InfoWindow;
-    
-    var latLng = new google.maps.LatLng("37.803970", "-122.413146");
-    var marker = new google.maps.Marker({
-      position: latLng,
-      map: gmap
-    });
-    
-    var latLng2 = new google.maps.LatLng("37.788693", "-122.417545");
-    var marker2 = new google.maps.Marker({
-      position: latLng2,
-      map: gmap
-    });
-
-    gmap["markers"] = [marker, marker2]; 
+    var gmap = new google.maps.Map(map, mapOptions); 
     debugger 
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-        console.log(pos); 
-        infoWindow.setPosition(pos);
-        infoWindow.setContent('Location found.');
-        infoWindow.open(map);
-        map.setCenter(pos);
-      }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-      });
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
-  
-
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-      infoWindow.setPosition(pos);
-      infoWindow.setContent(browserHasGeolocation ?
-                            'Error: The Geolocation service failed.' :
-                            'Error: Your browser doesn\'t support geolocation.');
-      infoWindow.open(map);
-    }
+    gmap["markers"] = []; 
     
+    if (newProps.businesses) {
+      newProps.businesses.forEach( (business,idx) => {
+        debugger 
+        var latLng = new google.maps.LatLng(business.coordinates.latitude, business.coordinates.longitude);
+        var marker = new google.maps.Marker({
+          position: latLng,
+          map: gmap
+        });
+      gmap["markers"].push(marker);   
+      }); 
+    }
   }
 
-
   render() {
+    debugger 
     return (
       <div className="map" ref="map">
         Map
--- frontend/components/search/results_index_item.js
+++ frontend/components/search/results_index_item.js
@@ -5,7 +5,6 @@ class ResultsIndexItem extends React.Component {
 
   render(){
     const { business } = this.props; 
-
     if (!business){
       return null; 
     }
