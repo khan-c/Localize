6999770811869a68a7b543daa7bee859764cc872
Angel Velasco <avelasco920@gmail.com>
2017-12-16 21:23:40 -0800

    dispatch search when triggering search from mounted search page

--- frontend/components/search/map.jsx
+++ frontend/components/search/map.jsx
@@ -1,9 +1,9 @@
-import React from 'react'; 
+import React from 'react';
 
 
 class Map extends React.Component {
   componentDidMount() {
-   
+
     const mapOptions = {
       center: {
         lat: 37.773972,
@@ -12,25 +12,24 @@ class Map extends React.Component {
       zoom: 13
     };
 
-    const map = this.refs.map; 
-    //creates new map 
-    let gmap = new google.maps.Map(map, mapOptions);    
+    const map = this.refs.map;
+    //creates new map
+    let gmap = new google.maps.Map(map, mapOptions);
     let infoWindow = new google.maps.InfoWindow;
-    
+
     var latLng = new google.maps.LatLng("37.803970", "-122.413146");
     var marker = new google.maps.Marker({
       position: latLng,
       map: gmap
     });
-    
+
     var latLng2 = new google.maps.LatLng("37.788693", "-122.417545");
     var marker2 = new google.maps.Marker({
       position: latLng2,
       map: gmap
     });
 
-    gmap["markers"] = [marker, marker2]; 
-    debugger 
+    gmap["markers"] = [marker, marker2];
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
@@ -38,7 +37,7 @@ class Map extends React.Component {
           lat: position.coords.latitude,
           lng: position.coords.longitude
         };
-        console.log(pos); 
+        console.log(pos);
         infoWindow.setPosition(pos);
         infoWindow.setContent('Location found.');
         infoWindow.open(map);
@@ -50,7 +49,7 @@ class Map extends React.Component {
       // Browser doesn't support Geolocation
       handleLocationError(false, infoWindow, map.getCenter());
     }
-  
+
 
     function handleLocationError(browserHasGeolocation, infoWindow, pos) {
       infoWindow.setPosition(pos);
@@ -59,7 +58,7 @@ class Map extends React.Component {
                             'Error: Your browser doesn\'t support geolocation.');
       infoWindow.open(map);
     }
-    
+
   }
 
 
@@ -69,8 +68,8 @@ class Map extends React.Component {
         Map
 
       </div>
-    ); 
+    );
   }
 }
 
-export default Map; 
+export default Map;
--- frontend/components/search/search_page.jsx
+++ frontend/components/search/search_page.jsx
@@ -1,41 +1,65 @@
-import React from 'react'; 
-import ResultsWrapper from './results_wrapper'; 
-import MapContainer from './map_container'; 
-import merge from 'lodash/merge'; 
-// import ResultsWrapperContainer from './results_wrapper_container'; 
-import {fetchLatLng} from '../../util/location_util'; 
-import { superSearch } from '../../actions/search_actions'; 
+import React from 'react';
+import ResultsWrapper from './results_wrapper';
+import MapContainer from './map_container';
+import merge from 'lodash/merge';
+// import ResultsWrapperContainer from './results_wrapper_container';
+import {fetchLatLng} from '../../util/location_util';
+import { superSearch } from '../../actions/search_actions';
 import { urlToQuery } from '../../util/parsing_functions';
 
 const defaultQuery = {
-  term: "Chinese", 
-  // latitude: "37.786882",  
-  // longitude: "-122.399972", 
+  term: "Chinese",
+  // latitude: "37.786882",
+  // longitude: "-122.399972",
   location: "Manhattan",
-  category: "" 
-}; 
+  category: ""
+};
 
 class SearchPage extends React.Component {
   componentDidMount(){
-
-    // pulls URL from history and converts to a query object 
-    let searchQuery = urlToQuery(this.props.location.search); 
+    // pulls URL from history and converts to a query object
+    let searchQuery = urlToQuery(this.props.location.search);
     if (searchQuery.location === "") {
-      searchQuery.location = "San Francisco"; 
+      searchQuery.location = "San Francisco";
     }
-    
     // Get Latitude and Longitude from Google Maps Geocode API from the location provided by search
     fetchLatLng(searchQuery.location).then(
       results => {
-        let coordinates = results.data.results[0].geometry.location; 
-        let query = merge({}, searchQuery, {term: searchQuery.text} ); 
-        query["latitude"] = coordinates.lat; 
-        query["longitude"] = coordinates.lng; 
-        delete query["location"]; 
-        this.props.getSearch(query); 
-      }); 
+        let coordinates = results.data.results[0].geometry.location;
+        let query = merge({}, searchQuery, {term: searchQuery.text} );
+        query["latitude"] = coordinates.lat;
+        query["longitude"] = coordinates.lng;
+        delete query["location"];
+        this.props.getSearch(query);
+      }
+    );
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (newProps.location !== this.props.location) {
+      // pulls URL from history and converts to a query object
+      let searchQuery = urlToQuery(newProps.location.search);
+      if (searchQuery.location === "") {
+        searchQuery.location = "San Francisco";
+      }
+      // Get Latitude and Longitude from Google Maps Geocode API from the location provided by search
+      fetchLatLng(searchQuery.location).then(
+        results => {
+          let coordinates = results.data.results[0].geometry.location;
+          let query = merge({}, searchQuery, {term: searchQuery.text} );
+          query["latitude"] = coordinates.lat;
+          query["longitude"] = coordinates.lng;
+          delete query["location"];
+          this.props.getSearch(query);
+        }
+      );
     }
-  
+  }
+
+  componentWillUnmount() {
+    this.props.clearSearchResults();
+  }
+
   render() {
     return(
       <div className="search-page-wrapper">
@@ -44,9 +68,9 @@ class SearchPage extends React.Component {
           <MapContainer />
         </div>
       </div>
-    ); 
+    );
   }
 }
 
 
-export default SearchPage; 
+export default SearchPage;
