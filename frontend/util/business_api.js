6697f9cfce2e0a8e1611ff3841a8bdcccad36acf
Kyle Chen <kyle.h.chen@gmail.com>
2017-12-18 04:53:56 -0800

    Update api key

--- frontend/util/business_api.js
+++ frontend/util/business_api.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
+import { mongoConfig } from '../../backend/server/config/keys';
 
 export const createBusiness = formBusiness => {
   console.log("business_api createBusiness");
   // return axios.post('http://localhost:8000/api/businesses', { formBusiness });
   return axios.post(
-    'https://api.mlab.com/api/1/databases/localize/collections/businesses?apiKey=${}',
+    'https://api.mlab.com/api/1/databases/localize/collections/businesses?apiKey=${mongoConfig.apikey}',
     { formBusiness });
 };
 
@@ -16,5 +17,5 @@ export const fetchBusiness = businessId => {
   const name = businessId.slice(0,-9);
   // return axios.get(`/api/businesses/${name}`);
   const query = JSON.stringify({name});
-  return axios.get(`https://api.mlab.com/api/1/databases/localize/collections/businesses?q={"name":"${name}"}&fo=true&apiKey=RSX9tQBueBgDG6bL6D5kF1jsGdtXWSzX`)
+  return axios.get(`https://api.mlab.com/api/1/databases/localize/collections/businesses?q={"name":"${name}"}&fo=true&apiKey=${mongoConfig.apikey}`)
 };
