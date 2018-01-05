971abb80dc914e5658fbabae4273ecb86cd16637
Truong Nguyen <tmnguyen12@gmail.com>
2018-01-04 18:34:13 -0800

    Add a comment for comparing files

--- backend/server/index.js
+++ backend/server/index.js
@@ -86,7 +86,7 @@ app.get('/search', (req, res) => {
   });
 });
 
-
+// Adding this here for testing
 // route and controller for autocomplete
 app.get('/autocomplete', (req, res) => {
   let autoUrl = "https://api.yelp.com/v3/autocomplete?";
