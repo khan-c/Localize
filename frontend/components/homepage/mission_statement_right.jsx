80901a38ad874f42fdc03b9a4cbed63af155e825
Angel Velasco <avelasco920@gmail.com>
2018-01-06 21:54:48 -0800

    center navbar when page gets smaller

--- frontend/components/homepage/mission_statement_right.jsx
+++ frontend/components/homepage/mission_statement_right.jsx
@@ -14,7 +14,7 @@ const MissionStatementRight = () => (
       />
     </div>
     <div className='mission-text-wrapper'>
-      <h2 className='mission-right-title mission-title'>Commission Local Artist and Makers</h2>
+      <h2 className='mission-right-title mission-title'>Commission Local Artists and Makers</h2>
       <span className='mission-text display-text'>
         We have seen a large increase in commissioned pieces from companies
         looking to fill their offices with paintings, murals, sculptures, and large center-pieces from
